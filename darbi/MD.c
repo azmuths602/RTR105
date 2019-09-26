499d4f6e9b55abb3f07778c92122b5fc66a5be12
azmuths602 <maksimgavlas@gmail.com>
2019-09-26 18:51:42 +0300

    20192609_18_51

---
+++ darbi/MD.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+ float pi(float a) {
+}
+
+ int main() {
+ 	int R=123 ; 
+	float num=3.14 ;
+	float L=2*pi(num)*R ;
+ 	printf("Rinka ar raadiusu %d garums ir %.2f m", R, L);
+ 	return 0;
+} 
