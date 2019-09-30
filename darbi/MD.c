6c91d67c01e1e967191ad5fed264fd94f474b0a7
user <maksimgavlas@gmail.com>
2019-09-30 15:04:10 +0300

    20190930_15_04

--- darbi/MD.c
+++ darbi/MD.c
@@ -5,8 +5,11 @@
 
  int main() {
  	int R=123 ; 
-	float num=3.14 ;
-	float L=2*pi(num)*R ;
- 	printf("Rinka ar raadiusu %d garums ir %.2f m", R, L);
+	float num=3.14;
+	printf ("%.2f\n", num);
+	float K=2*pi(num);
+	printf("%.2f\n", K);
+	float L=K*R ;
+ 	printf("Rinka ar raadiusu %d garums ir %.2f m\n", R, L);
  	return 0;
 } 
